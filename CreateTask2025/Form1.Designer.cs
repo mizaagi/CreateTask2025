4ca4e8884ec7451db10264691757f9ecea8d1b59
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-03-12 10:39:36 -0500

    3/12/2025

--- CreateTask2025/Form1.Designer.cs
+++ CreateTask2025/Form1.Designer.cs
@@ -28,80 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.Label prompt;
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            prompt = new System.Windows.Forms.Label();
+            System.Windows.Forms.Label promptLabel;
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.submitAnswerButton = new System.Windows.Forms.Button();
+            this.editFlashcardsButton = new System.Windows.Forms.Button();
+            this.rightWrong = new System.Windows.Forms.PictureBox();
+            this.flashcardsListBox = new System.Windows.Forms.ListBox();
+            this.newTermButton = new System.Windows.Forms.Button();
+            this.enterInfoBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.submitNewButton = new System.Windows.Forms.Button();
+            promptLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rightWrong)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // promptLabel
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 397);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(776, 20);
-            this.textBox1.TabIndex = 0;
+            promptLabel.AutoSize = true;
+            promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            promptLabel.Location = new System.Drawing.Point(332, 195);
+            promptLabel.Name = "promptLabel";
+            promptLabel.Size = new System.Drawing.Size(101, 31);
+            promptLabel.TabIndex = 1;
+            promptLabel.Text = "Prompt";
             // 
-            // prompt
+            // answerBox
             // 
-            prompt.AutoSize = true;
-            prompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            prompt.Location = new System.Drawing.Point(355, 45);
-            prompt.Name = "prompt";
-            prompt.Size = new System.Drawing.Size(101, 31);
-            prompt.TabIndex = 1;
-            prompt.Text = "Prompt";
+            this.answerBox.Location = new System.Drawing.Point(12, 397);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(776, 20);
+            this.answerBox.TabIndex = 0;
             // 
-            // button1
+            // submitAnswerButton
             // 
-            this.button1.Location = new System.Drawing.Point(338, 423);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Submit Answer";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.submitAnswerButton.Location = new System.Drawing.Point(338, 423);
+            this.submitAnswerButton.Name = "submitAnswerButton";
+            this.submitAnswerButton.Size = new System.Drawing.Size(118, 23);
+            this.submitAnswerButton.TabIndex = 2;
+            this.submitAnswerButton.Text = "Submit Answer";
+            this.submitAnswerButton.UseVisualStyleBackColor = true;
+            this.submitAnswerButton.Click += new System.EventHandler(this.submitAnswerButton_Click);
             // 
-            // button2
+            // editFlashcardsButton
             // 
-            this.button2.Location = new System.Drawing.Point(13, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Edit Flashcards";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.editFlashcardsButton.Location = new System.Drawing.Point(13, 13);
+            this.editFlashcardsButton.Name = "editFlashcardsButton";
+            this.editFlashcardsButton.Size = new System.Drawing.Size(94, 23);
+            this.editFlashcardsButton.TabIndex = 3;
+            this.editFlashcardsButton.Text = "Edit Flashcards";
+            this.editFlashcardsButton.UseVisualStyleBackColor = true;
+            this.editFlashcardsButton.Click += new System.EventHandler(this.editFlashcardsButton_Click);
             // 
-            // progressBar1
+            // rightWrong
             // 
-            this.progressBar1.Location = new System.Drawing.Point(125, 12);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(628, 23);
-            this.progressBar1.TabIndex = 4;
+            this.rightWrong.Location = new System.Drawing.Point(688, 13);
+            this.rightWrong.Name = "rightWrong";
+            this.rightWrong.Size = new System.Drawing.Size(100, 50);
+            this.rightWrong.TabIndex = 6;
+            this.rightWrong.TabStop = false;
             // 
-            // comboBox1
+            // flashcardsListBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 41);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 5;
+            this.flashcardsListBox.FormattingEnabled = true;
+            this.flashcardsListBox.Location = new System.Drawing.Point(13, 42);
+            this.flashcardsListBox.Name = "flashcardsListBox";
+            this.flashcardsListBox.Size = new System.Drawing.Size(120, 95);
+            this.flashcardsListBox.TabIndex = 8;
+            this.flashcardsListBox.DoubleClick += new System.EventHandler(this.flashcardsListBox_DoubleClick);
+            // 
+            // newTermButton
+            // 
+            this.newTermButton.Location = new System.Drawing.Point(13, 144);
+            this.newTermButton.Name = "newTermButton";
+            this.newTermButton.Size = new System.Drawing.Size(75, 23);
+            this.newTermButton.TabIndex = 9;
+            this.newTermButton.Text = "New Term";
+            this.newTermButton.UseVisualStyleBackColor = true;
+            this.newTermButton.Click += new System.EventHandler(this.newTermButton_Click);
+            // 
+            // enterInfoBox
+            // 
+            this.enterInfoBox.Location = new System.Drawing.Point(12, 190);
+            this.enterInfoBox.Name = "enterInfoBox";
+            this.enterInfoBox.Size = new System.Drawing.Size(100, 20);
+            this.enterInfoBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 11;
+            // 
+            // submitNewButton
+            // 
+            this.submitNewButton.Location = new System.Drawing.Point(119, 190);
+            this.submitNewButton.Name = "submitNewButton";
+            this.submitNewButton.Size = new System.Drawing.Size(75, 23);
+            this.submitNewButton.TabIndex = 12;
+            this.submitNewButton.Text = "Submit";
+            this.submitNewButton.UseVisualStyleBackColor = true;
+            this.submitNewButton.Click += new System.EventHandler(this.submitNewButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(prompt);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.submitNewButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enterInfoBox);
+            this.Controls.Add(this.newTermButton);
+            this.Controls.Add(this.flashcardsListBox);
+            this.Controls.Add(this.rightWrong);
+            this.Controls.Add(this.editFlashcardsButton);
+            this.Controls.Add(this.submitAnswerButton);
+            this.Controls.Add(promptLabel);
+            this.Controls.Add(this.answerBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rightWrong)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,11 +156,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button submitAnswerButton;
+        private System.Windows.Forms.Button editFlashcardsButton;
+        private System.Windows.Forms.PictureBox rightWrong;
+        private System.Windows.Forms.ListBox flashcardsListBox;
+        private System.Windows.Forms.Button newTermButton;
+        private System.Windows.Forms.TextBox enterInfoBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button submitNewButton;
     }
 }
 
--- CreateTask2025/Form1.cs
+++ CreateTask2025/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,20 +13,60 @@ namespace CreateTask2025
 {
     public partial class Form1 : Form
     {
+        private String termOrDef = "term";
+        private ArrayList definitions = new ArrayList();
         public Form1()
         {
             InitializeComponent();
+            flashcardsListBox.Visible = false;
+            newTermButton.Visible = false;
+            label1.Text = "";
+            enterInfoBox.Visible = false;
+            submitNewButton.Visible = false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void submitAnswerButton_Click(object sender, EventArgs e)
         {
 
-            textBox1.Clear();
+            answerBox.Clear();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void editFlashcardsButton_Click(object sender, EventArgs e)
         {
-            comboBox1.DroppedDown = true;
+            flashcardsListBox.Visible = !flashcardsListBox.Visible;
+            newTermButton.Visible = !newTermButton.Visible;
+            flashcardsListBox.Text = "";
+        }
+
+        // https://stackoverflow.com/questions/4454423/c-sharp-listbox-item-double-click-event
+        private void flashcardsListBox_DoubleClick(object sender, EventArgs e)
+        {
+            // termOrDef =
+        }
+
+        private void newTermButton_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Enter term:";
+            enterInfoBox.Visible = true;
+            submitNewButton.Visible = true;
+        }
+
+        private void submitNewButton_Click(object sender, EventArgs e)
+        {   
+            if (termOrDef.Equals("term")) {
+                flashcardsListBox.Items.Add(enterInfoBox.Text);
+                enterInfoBox.Text = "";
+                label1.Text = "Enter definition:";
+                termOrDef = "definition";
+            } else if (termOrDef.Equals("definition"))
+            {
+                definitions.Add(enterInfoBox.Text);
+                label1.Text = "";
+                submitNewButton.Visible = false;
+                enterInfoBox.Visible = false;
+            }
+            
+
         }
     }
 }
